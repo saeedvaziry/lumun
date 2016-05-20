dfb49c0030568d13aabbc5a22bb351d32dceae50
Saeed Vaziry <sa.vaziry@gmail.com>
2016-05-20 23:46:08 +0430

    first init

---
+++ app/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+$router->respond(function () {
+    return 'All the things';
+});
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once __DIR__.'/../core/bootstrap/autoload.php';
+
+spl_autoload_register(function ($class_name) {
+    include __DIR__.'/../'.$class_name . '.php';
+});
+
+$router = new \Klein\Klein();
+include_once __DIR__.'/../app/routes.php';
+$router->dispatch();
